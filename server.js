49f17340d06a4c973683d91068be273ed45a0dc7
Aliel Reyes <alielreyes@gmail.com>
2020-09-10 16:48:42 -0700

    Served my react app through express.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const axios = require('axios');
+const path = require("path");
+
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-    const customers = [
-        {id: 1, firstName: "Jonh", lastName: "Doe"},
-        {id: 2, firstName: "Luke", lastName: "Cage"},
-        {id: 3, firstName: "Tony", lastName: "Stark"}
-    ];
+//Serving react via express and node.js, You have to serve the build. 
+app.use('/', express.static(path.join(__dirname + "/client/build")));
 
-    res.json(customers);
-})
+app.get("/swapi", (req, res) => {
+    axios.get("http://swapi.dev/api/people/").then(response => {
+        res.send(response.data.results);
+    })
+});
 
-const port = 5001;
-
+const port = 5003;
 app.listen(port, () => console.log(`Server started on port ${port}`));
